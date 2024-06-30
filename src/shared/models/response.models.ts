65961abdc12a9ca6be972509172768dc3cbc58c7
rdevgc <rubengc1799@gmail.com>
2024-06-30 22:01:06 +0200

    FEAT: response estandarization

--- src/shared/models/response.models.ts
+++ src/shared/models/response.models.ts
@@ -1,4 +1,13 @@
+import { Response } from 'express';
+
 export interface ResponseDTO<T = null> {
   ok: boolean;
   data: T;
 }
+
+export const getResponse = <T>(res: Response, data: T): Response<ResponseDTO<T>> => {
+  return res.json({
+    ok: true,
+    data,
+  });
+};
