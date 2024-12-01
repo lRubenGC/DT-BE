5dfbd4a096e4589f7fad972ac976510b0f55c28b
rdevgc <rubengc1799@gmail.com>
2024-12-01 23:15:09 +0100

    FIX: allow undefined year in basic cars

--- src/shared/middlewares/validate-basic-cars.ts
+++ src/shared/middlewares/validate-basic-cars.ts
@@ -1,7 +1,7 @@
 import { ERROR } from '../models/errors.models';
 
 export const isValidYear = async (year: number) => {
-  if (!year || typeof year !== 'number' || year < 1950 || year > 2050) {
+  if (year && (typeof year !== 'number' || year < 1950 || year > 2050)) {
     throw new Error(ERROR.BAD_PAYLOAD.toString());
   }
 };
